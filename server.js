aec2507c59f298e12d224e8a0d269e33b4eac4cd
Min Khant Hein <minkhanthein.15-16@ucsmub.edu.mm>
2020-10-22 21:24:10 +0630

    second commit

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require ('express');
 const app = express();
 const path = require ('path');
+const mongoose = require ('mongoose');
+
+mongoose.connect('mongodb+srv://mkh:123@cluster0.dgz0g.mongodb.net/<dbname>?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true },()=>{
+  console.log("DB is connected");
+})
 
 app.use (express.static(__dirname + '/dist/Appointment'));
 app.get("/",(req,res)=>{
   res.sendFile(path.join(__dirname+ '/dist/Appointment/index.html'))
 })
+
+const appRouter = require('./Back-end/router/route');
+app.use('/appointment',appRouter);
 
 app.listen(process.env.PORT || 8080,()=>{
   console.log("server is running on port 8080")
